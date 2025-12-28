917396257a74fb2bd2381c68079e6ae84c03a418
MariamRazmadze <mariam.razmadze.1@iliauni.edu.ge>
2025-12-28 14:35:26 +0400

    add home and about pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Albert_Sans, Montserrat_Alternates } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const albertSans = Albert_Sans({ subsets: ["latin"], display: "swap" });
+const montserratAlternates = Montserrat_Alternates({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  display: "swap",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-montserrat-alternates",
 });
 
 export const metadata: Metadata = {
@@ -25,8 +23,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${albertSans.className} ${montserratAlternates.variable}`}
       >
+        <header>
+          <nav>
+            <p>Logo</p>
+            <p>3D Models</p>
+            <p>About</p>
+          </nav>
+        </header>
         {children}
       </body>
     </html>
